f32d74904548c9c98d0464bf0c235b76797890a3
Shalu Ambasta <itsambastakashish@gmail.com>
2023-07-13 16:17:02 +0530

    users detail component added 😊

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "../css/UserDetail.css";
+import { FaUserSecret } from "react-icons/fa";
 
 function UserDetail() {
   return (
-    <div>
-      <div>UserDetail</div>
+    <div className="user__detail">
+      <div className="user__header">
+        <FaUserSecret className="user__icon" />
+        <h1 className="nowrap">Users</h1>
+      </div>
+      <div className="user__names">
+        <p>Lorem Ipsum</p>
+      </div>
     </div>
   );
 }
