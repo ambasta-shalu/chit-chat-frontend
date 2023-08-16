178f0ae3332072195b5ff0e7e97e654d86e20a30
Shalu Ambasta <itsambastakashish@gmail.com>
2023-08-16 20:37:51 +0530

    deployed server url added :)

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,4 @@
-// FOR TESTING PURPOSE
-export const SERVER_DOMAIN = "http://localhost:7000";
+// // FOR TESTING PURPOSE
+// export const SERVER_DOMAIN = "http://localhost:7000";
+
+export const SERVER_DOMAIN = "https://chit-chat-bago.onrender.com/";
